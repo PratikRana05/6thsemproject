64e1805b7504be02fe552b50edc46afcc260132e
PratikRana05 <pratikrana029@gmail.com>
2026-01-05 07:43:21 +0545

    added model file

---
+++ projects/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Project(models.Model):
+    class StatusOptions(models.TextChoices):
+        PENDING = "Pending", "Pending"
+        IN_PROGRESS = "In Progress", "In Progress"
+        COMPLETED = "Completed", "Completed"
+        CANCELED = "Canceled", "Canceled"
+
+    title = models.CharField(max_length=25)
+    description = models.TextField(null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    status = models.CharField(
+        max_length=15,
+        choices=StatusOptions.choices,   # ✅ FIX HERE
+        default=StatusOptions.PENDING
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True, editable=False)
+    
+    def __str__(self):
+        return self.title
